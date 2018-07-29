713dc74fd7e38630fba6531140cdf79bbc31d44a
Dmitry Smolyakov <dmitry.smolyakov@lykke.com>
2018-07-29 06:53:24 +0300

    LWDEV-8684 New logging for client nuget.

--- client/Lykke.Service.KycReports.Client/AutofacExtension.cs
+++ client/Lykke.Service.KycReports.Client/AutofacExtension.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using Autofac;
 using Common.Log;
+using Lykke.Common.Log;
 
 namespace Lykke.Service.KycReports.Client
 {
     public static class AutofacExtension
     {
+        [Obsolete("Please, use the overload which consumes ILogFactory instead.")]
         public static void RegisterKycReportsClient(this ContainerBuilder builder, string serviceUrl, ILog log)
         {
             if (builder == null) throw new ArgumentNullException(nameof(builder));
@@ -15,6 +17,28 @@ namespace Lykke.Service.KycReports.Client
                 throw new ArgumentException("Value cannot be null or whitespace.", nameof(serviceUrl));
 
             builder.RegisterInstance(new KycReportsClient(serviceUrl, log)).As<IKycReportsClient>().SingleInstance();
+        }
+
+        public static void RegisterKycReportsClient(this ContainerBuilder builder, string serviceUrl,
+            ILogFactory logFactory)
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+            if (serviceUrl == null)
+                throw new ArgumentNullException(nameof(serviceUrl));
+            if (logFactory == null)
+                throw new ArgumentNullException(nameof(logFactory));
+            if (string.IsNullOrWhiteSpace(serviceUrl))
+                throw new ArgumentException("A reachable Lykke.Service.KycReports URL is required but null or empty string was given.", nameof(serviceUrl));
+
+            builder.RegisterInstance(
+                new KycReportsClient(
+                    serviceUrl, 
+                    logFactory.CreateLog("KycReportsClient")
+                    )
+                )
+                .As<IKycReportsClient>()
+                .SingleInstance();
         }
     }
 }
